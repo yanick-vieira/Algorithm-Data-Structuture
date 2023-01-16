ca21028da961a5ed267475bb2366101da399cd30
yanick-vieira <yanickvieira7@gmail.com>
2023-01-16 13:30:13 +0000

    Update interact.js

--- Logic/interact.js
+++ Logic/interact.js
@@ -1,7 +1,11 @@
 
-function phrase(){
-
-   console.log("Hello World!");
+function phrase(x,y){
+   var sum = x + y;
+   return sum;
 }
 
-phrase();
+var res = phrase(33,45.34);
+var res1 = phrase(20,10);
+
+console.log(res);
+console.log(res1);
