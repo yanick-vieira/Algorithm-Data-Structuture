eea601aa661a1e6aad122a2e231e081280a8e5a2
yanick-vieira <yanickvieira7@gmail.com>
2023-01-24 15:36:58 +0000

    arays

---
+++ Logic/interact.js
@@ -0,0 +1,20 @@
+//arrays - set of data joined
+/*
+inspecionar = [];
+
+inspecionar[0] = "1";
+inspecionar[1] = "2";
+inspecionar[2] = "3";
+inspecionar[7] = "8";
+inspecionar[4] = 25;
+inspecionar[3] = (Number.parseInt(inspecionar[7]) + Number.parseInt(inspecionar[1]));
+inspecionar[5] = ["YOK","POK","JO","KEIKS"];
+
+console.log(inspecionar[5][2]);
+*/
+
+//practice
+
+
+
+
