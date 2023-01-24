fe65ffed75f83e7f873b0d19b601fbd8849b37f6
yanick-vieira <yanickvieira7@gmail.com>
2023-01-24 17:05:50 +0000

    arrays

--- Logic/interact.js
+++ Logic/interact.js
@@ -15,6 +15,35 @@ console.log(inspecionar[5][2]);
 
 //practice
 
+function MovimentodeMarketing(){
 
+Serviço = ["Redes Sociais" , " Linha Editorial" , " Trafego Pago" , " Landing Page" , " Email Marketing"];
+
+Subserviço = [ ["Posts"," Conteúdos de Imagem", " Anuncios Pago" ],  ["Copy", " Blog", " Call to Action"], [" Google Ads"," Youtube Ads"," Google Analytics"], [" Design"," Conteudo Page"," Posicionamento"], [" Conteudo Email", " Anuncios Pagos", " Copywritting"] ];
+
+serviçoselecionado = 1;
+
+
+contratar = Serviço[serviçoselecionado];
+servir = Subserviço[serviçoselecionado];
+
+
+console.log("A minha minha Empresa quer o serviço de " + contratar + " na area de " + servir + "!");
+
+}
+
+var serviços = "serviços"
+
+
+if(serviços === "serviços")
+{
+
+    console.log(MovimentodeMarketing());
+
+}else
+
+{
+   console.log("A palavra chave = serviços");
+};
 
 
