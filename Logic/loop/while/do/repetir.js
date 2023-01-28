67c0acbce8d717dc02af19074190006beb7127ff
yanick-vieira <yanickvieira7@gmail.com>
2023-01-28 15:08:02 +0000

    loop arrays

---
+++ Logic/loop/while/do/repetir.js
@@ -0,0 +1,35 @@
+/*
+//Loop
+var repetir = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10];
+
+for(var i = 0; i < repetir.length; i++){
+
+    console.log(repetir[i]);
+    if(i === 0){
+       continue;
+       
+    }; 
+};
+
+
+
+//While
+var num = 1;
+
+while(num <= 10){
+    console.log(num++);
+
+};
+*/
+
+//Do while
+/*
+var num = 10;
+
+do{
+    console.log(num++);
+}
+while(num < 15);
+*/
+
+
