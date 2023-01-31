25be54b972fbd9b53c968d22a93d0008b3676dbd
yanick-vieira <yanickvieira7@gmail.com>
2023-01-31 15:50:10 +0000

    modificações

---
+++ Logic/Procedimentos/funções.js
@@ -0,0 +1,22 @@
+/*
+function trabalhar(){
+    console.log("Pegar o papel");
+    console.log("Pegar a caneta");
+    console.log("Sentar no estudio");
+    console.log("Desenhar");
+};
+
+trabalhar();
+*/
+
+/*
+function animal(aves, roedores){
+  console.log("Passou uma "+ aves);
+  console.log("Encontrei um "+ roedores);
+};
+
+animal("Gaivota","Rato");
+*/
+
+
+
