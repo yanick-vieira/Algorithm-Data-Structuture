8aa262054af87658dbc14036f0d4b8fc8326ccf4
yanick-vieira <yanickvieira7@gmail.com>
2023-02-01 17:07:33 +0000

    function if

--- Logic/Procedimentos/funções.js
+++ Logic/Procedimentos/funções.js
@@ -19,3 +19,30 @@ animal("Gaivota","Rato");
 */
 
 
+// Calculo de IMC
+
+function imc (x){
+
+ if( (x >= 11) && (x <= 21) ) {
+
+    console.log("Parabens, voçe está dentro da média saudável!");
+
+ }else if((x >= 22) && (x <= 32)){
+
+    console.log("Voçe precisa fazer mais exercícios físicos, pois está acima da média!");
+
+ }else if((x >= 33) && (x <= 900)){
+
+  console.log ("Voçe está a correr rísco de vida, precisa consultar um médico urgente!");
+  
+ 
+}else{
+    alert("Tente outro número real, Por favor!");
+};
+
+   return x;
+};
+
+imc(21);
+
+
