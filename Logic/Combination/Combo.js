622a890369f6c92fc9851afe0558b18eaa57baaf
yanick-vieira <yanickvieira7@gmail.com>
2023-02-04 15:38:48 +0000

    combo 2

--- Logic/Combination/Combo.js
+++ Logic/Combination/Combo.js
@@ -1,6 +1,19 @@
-let codigo = document.getElementsByTagName('span')[0];
 
-let amarelado = codigo.style.color = "#aaa900";
 
-console.log(codigo);
+let nome = "Yanick"
+let altura = 1.72;
+let vezes = altura * altura;
+let peso = 67;
+
+let imc = peso / vezes;
+
+
+if(imc > 24){
+    console.log(nome + " você se encontra acima do peso")
+}else if(imc < 18){
+    console.log(nome +" você se encontra abaixo do peso")
+}else{
+    console.log(nome + " você encontra dentro do peso, parabens")
+};
+console.log("IMC igual a " + imc.toFixed(2));
 
